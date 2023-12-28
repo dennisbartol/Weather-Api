ce14b7e3eb91368f2ad71e4f143646ec9b0b3018
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-28 14:35:14 +0100

    Update App.vue
    -Added in, CSS in App.vue.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <main>
        <div class="search-box">
-         <input type="text" class="search-bar">
+         <input type="text" class="search-bar" placeholder="Search..." />
+         
        </div>
     </main>  
   </div>
@@ -21,5 +22,45 @@ export default {
 
 
 <style>
+  * {
+    margin: 0; 
+    padding: 0; 
+    box-sizing: border-box;
+  }
 
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+#app { 
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover; 
+  background-position: bottom: 
+  transition: 0.5s; 
+}
+
+main { 
+  min-height: 100vh; 
+  padding: 22px; 
+
+  background-image: linear-gradient(to bottom, #222, #333);
+  }
+
+  .search-box {
+   width: 90%; 
+    margin-bottom: 25px; 
+  }
+
+  .search-box .search-bar { 
+   display: block; 
+   width: 90%; 
+   padding: 12px; 
+   color: #313131; 
+   font-size: 1.5em;
+   border: none; 
+   outline: none; 
+
+}
+  
+  
 </style>
