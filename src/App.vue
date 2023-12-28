6dfc922c77c65f8755510b911cc3d7e637d6229c
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-28 18:32:34 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
        <div class="weather-wrap" v-if="typeof weather.main != 'undefined' ">
         <div class="location-box">
           <div class="location"> {{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date"></div>
+          <div class="date"> {{ dateBuild(); }} </div>
        </div>  
 
       <div class="weather-box">
