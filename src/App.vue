2a77511c1a417c7629c6289aa83b633a07da8b54
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2023-12-29 16:57:51 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -138,5 +138,28 @@ main {
 .weather-box {
   text-align: center;
 }  
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: ghostwhite;
+  font-size: 6em;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: ghostwhite;
+  font-size: 2.5em;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}  
   
 </style>
