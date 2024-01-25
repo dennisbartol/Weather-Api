d3651351733f8fa4548e61902410e918ca20cc35
D. Bartol <85198215+dennisbartol@users.noreply.github.com>
2024-01-25 20:19:44 +0100

    Update main.js
    -Added VueRouter in main.js.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/forecast', component: Forecast }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+
 Vue.config.productionTip = false 
 
 new Vue ({
